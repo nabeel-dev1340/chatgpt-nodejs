5f6827e7142139055b4380a57d2571acf140f94d
nabeel-dev1340 <nabeelsharafat@gmail.com>
2023-02-06 21:28:26 +0500

    chatgpt client node js

---
+++ index.js
@@ -0,0 +1,28 @@
+import { ChatGPTAPI } from "chatgpt";
+import dotenv from "dotenv";
+import Express from "express";
+import bodyParser from "body-parser";
+
+dotenv.config();
+
+const app = Express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+  const TIME = req.query.time;
+  const LOCATION = req.query.location;
+  const EQUIPMENT = req.query.equipment;
+  const MUSCLE = req.query.muscle;
+
+  const Prompt = `Give me a ${TIME} minute workout plan for ${MUSCLE} at ${LOCATION} with ${EQUIPMENT}. Please include a warmup and cooldown. Also specify the time period for each exercise.`;
+  const api = new ChatGPTAPI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const data = await api.sendMessage(Prompt);
+  res.send(data.text);
+});
+
+app.listen(3000, () => {
+  console.log("app listening on port 3000!");
+});
