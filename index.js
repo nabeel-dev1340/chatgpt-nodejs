42c21d4d4096fc1d3d4ff891fff6c2e97c7ed551
nabeel-dev1340 <nabeelsharafat@gmail.com>
2023-02-10 16:50:47 +0500

    edited prompt

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ dotenv.config();
 
 const API_KEY = process.env.OPENAI_API_KEY;
 const MODEL = "text-davinci-003";
-const allowedOrigins = ["https://wild-gray-goshawk-wrap.cyclic.app","http://localhost:3000"];
+const allowedOrigins = [
+  "https://wild-gray-goshawk-wrap.cyclic.app",
+  "http://localhost:3000",
+];
 
 const app = Express();
 app.use(bodyParser.json());
@@ -34,7 +37,7 @@ app.get("/", async (req, res) => {
 
   try {
     const Prompt = `
-  Give me a ${TIME} minute workout plan for ${MUSCLE} at ${LOCATION} with ${EQUIPMENT}. Please include a warmup and cooldown. Also specify the time period for each exercise. Give the results in json format with keys Warm up:  Exercises:  and Cool down. All these keys have array entry`;
+  Give me a ${TIME} minute workout plan for ${MUSCLE} at ${LOCATION} with ${EQUIPMENT}. Please include a warmup and cooldown. Also specify the time period for each exercise. Give the results in json format with keys Warm up:  Exercises:  and Cool down. All these keys have array entry and please provide a valid json object`;
     const response = await axios.post(
       "https://api.openai.com/v1/completions",
       {
